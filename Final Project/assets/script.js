6ece279568bd51868fa0f3b51d126bf81c297f67
Malith-k <149454370+Malith-k@users.noreply.github.com>
2023-10-31 17:18:41 +0530

    Final-project

---
+++ Final Project/assets/script.js
@@ -0,0 +1,13 @@
+let menuIcon = document.querySelector('#icon-menu');
+let navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    
+    navbar.classList.toggle('.active');
+    
+}
+
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
